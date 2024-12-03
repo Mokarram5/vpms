dabbb7ce60c9058c10fb6c11d7f6f98ea2e4f805
Mokarram5 <mokarramkhn@gmail.com>
2024-12-04 00:11:19 +0530

    message

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bootstrap/vpms',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '59a6d743f86ac32406855d86fa46f3f28177d607',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bootstrap/vpms' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '59a6d743f86ac32406855d86fa46f3f28177d607',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
